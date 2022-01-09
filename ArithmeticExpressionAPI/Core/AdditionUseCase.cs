a61a713486b21d274e45a4ec5c0b893c6ac830ab
Eduardo Kuwakino <kuwakino@gmail.com>
2022-01-09 20:30:25 -0300

    refactoring(AdditionUseCase);

--- ArithmeticExpressionAPI/Core/AdditionUseCase.cs
+++ ArithmeticExpressionAPI/Core/AdditionUseCase.cs
@@ -7,7 +7,13 @@ using System.Text.RegularExpressions;
 namespace ArithmeticExpressionAPI.UseCases
 {
     public class AdditionUseCase
-    {
+    { 
+        private enum Action
+        {
+            AddAndSub,
+            AddOnly,
+        }
+
         private IExpressionValidatorService expressValidatorService;
         private IExpressionCalculatorService expressCalculatorService;
 
@@ -17,31 +23,40 @@ namespace ArithmeticExpressionAPI.UseCases
             this.expressCalculatorService = expressCalculatorService;
         }
 
-        public decimal Execute(string inExpression)
+        public decimal Execute(string inExpression) => ActionHandler(Action.AddAndSub, inExpression);
+
+        public decimal ExecuteAddOnly(string inExpression) => ActionHandler(Action.AddOnly, inExpression);
+
+        private decimal ActionHandler(Action action, string inExpression)
         {
             var result = 0m;
             var expression = RemoveWhiteSpaces(inExpression);
-            var isValidExpression = expressValidatorService.Validate(expression);
+
+            bool isValidExpression = IsValidExpression(action, expression);
+
             result = CalculateExpression(expression, isValidExpression);
 
             return result;
         }
 
-        public decimal ExecuteAddOnly(string inExpression)
+        private static string RemoveWhiteSpaces(string expression) => Regex.Replace(expression, @"\s+", "");
+
+        private bool IsValidExpression(Action action, string expression)
         {
-            var result = 0m;
-            var expression = RemoveWhiteSpaces(inExpression);
-            var isValidExpression = expressValidatorService.ValidateAddOnly(expression);
-            result = CalculateExpression(expression, isValidExpression);
+            var isValidExpression = false;
+            switch (action)
+            {
+                case Action.AddOnly:
+                    isValidExpression = expressValidatorService.ValidateAddOnly(expression);
+                    break;
+                default:
+                    isValidExpression = expressValidatorService.Validate(expression);
+                    break;
+            }
 
-            return result;
+            return isValidExpression;
         }
-
-        private static string RemoveWhiteSpaces(string expression)
-        {
-            return Regex.Replace(expression, @"\s+", "");
-        }
-
+        
         private decimal CalculateExpression(string inExpression, bool isValidExpression)
         {
             var result = 0m;
@@ -50,6 +65,7 @@ namespace ArithmeticExpressionAPI.UseCases
                 result = expressCalculatorService.Calculate(inExpression);
             else
                 throw new Exception("Invalid expression. Use infix notatation - operators are written between the operands (numbers) they operate on, e.g. 2 + 3 + 5.");
+            
             return result;
         }
     }
